9d18e9dc2499c5b37a614792889815878ba08aaf
washinson <sanya112001@yandex.ru>
2019-06-24 22:17:25 +0300

    add bluetooth default name

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,16 +1,19 @@
 LOCAL_PATH := device/meizu/m3
 
-# inherit from the proprietary version
+# Inherit from the proprietary version
 -include vendor/meizu/m3/BoardConfigVendor.mk
 
 include device/meizu/m3_m3s-common/BoardConfigCommon.mk
 
-#prebuilt kernel image
+# Prebuilt kernel image
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/kernel
 
-#system.prop
+# system.prop
 TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
 
 # Radio
 ADD_RADIO_FILES := true
 TARGET_RELEASETOOLS_EXTENSIONS := $(LOCAL_PATH)
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
